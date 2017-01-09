38d6c43af25ea2725a41fab1ac29cdd8cbf0144c
US1549 <ur1597532@163.com>
2017-01-09 18:37:18 +0800

    JavaFX Node通用属性及方法

---
+++ src/NodeStyleRotateDemo.java
@@ -0,0 +1,28 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+/**
+ * Created by Administrator on 2017/1/9.
+ */
+public class NodeStyleRotateDemo extends Application{
+    @Override
+    public void start(Stage primaryStage){
+        StackPane pane = new StackPane();
+        Button btOK = new Button("OK");
+        btOK.setStyle("-fx-border-color:blue;");
+
+        pane.getChildren().add(btOK);
+        pane.setRotate(45);
+        pane.setStyle("-fx-border-color: red;-fx-background-color: lightgray");
+
+        Scene scene = new Scene(pane,200,250);
+        primaryStage.setTitle("NodeStyleRotateDemo");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+}
